0d5e74b0c33f5da7b6c932f14d4e71bf7866a97b
xwb <xwb@qq.com>
2024-01-16 17:54:36 +0800

    feat:初始化项目

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import navBg from "@/assets/images/nav_bg.png";
+import logo from "@/assets/logo.png";
+import { Avatar, Menu, MenuProps } from "antd";
 interface NavBarProps {
   isFixed: boolean;
 }
@@ -12,10 +14,46 @@ const Container = styled.div<{ $isFixed?: boolean; $bgUrl?: string }>`
   background: #fff;
   background-image: url(${(props) => (props.$bgUrl ? props.$bgUrl : "none")});
 `;
+
+const items: MenuProps["items"] = [
+  {
+    label: "首页",
+    key: "home",
+  },
+  {
+    label: "学院概况",
+    key: "xueyuangaikuang",
+    children: [
+      {
+        label: "学院简介",
+        key: "description",
+      },
+      {
+        label: "学院新闻",
+        key: "news",
+      },
+      {
+        label: "学院动态",
+        key: "dongtai",
+      },
+      {
+        label: "学院荣誉",
+        key: "honour",
+      },
+    ],
+  },
+];
 export default function NavBar({ isFixed }: NavBarProps) {
   return (
     <Container $isFixed={isFixed} $bgUrl={navBg}>
-      导航栏
+      <div>
+        <div className="container">
+          <div className="logo" style={{ width: "60px", height: "60px" }}>
+            <Avatar src={<img src={logo} alt="logo" />} />
+          </div>
+        </div>
+        <Menu mode="horizontal" items={items} />
+      </div>
     </Container>
   );
 }
